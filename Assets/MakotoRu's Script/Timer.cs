4f369f82f0b78f47a002b43edfb6ffd82b2e1b82
胡凱鈞 <makotoru0@gmail.com>
2025-01-24 21:20:00 +0800

    Add Bubble and an arrow, arrow will flash with timer.

--- Assets/MakotoRu's Script/Timer.cs
+++ Assets/MakotoRu's Script/Timer.cs
@@ -1,22 +1,65 @@
+using Unity.Mathematics;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField]
     public float BubbleLifeTime = 10f;
-    // Update is called once per frame
+    [SerializeField]
+    private float LeftTime;
+
+    public GameObject Arrow;
+    void Start()
+    {
+        LeftTime = BubbleLifeTime;
+    }
     void Update()
     {
+        LeftTime -= Time.deltaTime;
         //
         //sound change with timer;
         //phase 1 => .    .    .
         //phase 2 => .  .  .
         //phase 3 => . . .
-        //
-        BubbleLifeTime -= Time.deltaTime;
-        if(BubbleLifeTime <= 0)
-        {
+
+        /// Shing Arrow
+        if(LeftTime > (BubbleLifeTime*2)/3){
+            if(Arrow != null){
+                if(math.sin(LeftTime*8)>0){
+                Arrow.SetActive(true);
+                }
+                else{
+                    Arrow.SetActive(false);
+                }
+        }
+            //play sound 1
+        }
+        else if(LeftTime > BubbleLifeTime/3){
+            if(Arrow != null){
+                if(math.sin(LeftTime*16)>0){
+                Arrow.SetActive(true);
+                }
+                else{
+                    Arrow.SetActive(false);
+                }
+            }
+            //play sound 2
+        }
+        else{
+            if(Arrow != null){
+                if(math.sin(LeftTime*30)>0){
+                Arrow.SetActive(true);
+                }
+                else{
+                    Arrow.SetActive(false);
+                }
+            }
+            //play sound 3
+        }
+        if(LeftTime <= 0){
             //Destroy the bubble
+            Destroy(gameObject);
         }
     }
 }
