ab99479ca5522a9145f61e93e7550b7314055ecf
Frank <tim4224171@gmail.com>
2025-01-26 06:08:12 +0800

    tidy asste and UI scripts

--- Assets/Scripts/Core/Judge.cs
+++ Assets/Scripts/Core/Judge.cs
@@ -2,22 +2,18 @@ using UnityEngine;
 
 public class Judge : MonoBehaviour
 {
-    [SerializeField]private SpriteRenderer SpriteRenderer;
-    [SerializeField]private Sprite Sprite1;
+    [SerializeField] private SpriteRenderer SpriteRenderer;
+    [SerializeField] private Sprite         Sprite1;
+    [SerializeField] private Sprite         Sprite2;
 
-    [SerializeField]private Sprite Sprite2;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log(other.gameObject.name);
         if (other.gameObject.tag == "Bubble")
         {
-            for(int i = 0; i < other.transform.parent.childCount; i++){
+            for (var i = 0; i < other.transform.parent.childCount; i++)
                 other.transform.parent.GetChild(i).GetComponent<BubbleStateController>().DestoryBubble();
-            }
 
-
-
-            ///judege 
             SpriteRenderer.sprite = Sprite1;
         }
     }
