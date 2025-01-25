82359d13058feedf8fed7020ace2df886ddf0def
Frank <tim4224171@gmail.com>
2025-01-25 15:53:28 +0800

    add button functions

--- Assets/Scripts/UI/ButtonTransition.cs
+++ Assets/Scripts/UI/ButtonTransition.cs
@@ -3,17 +3,18 @@ using UnityEngine.UI;
 
 namespace UI
 {
-    public class StartButton : MonoBehaviour
+    public class ButtonTransition : MonoBehaviour
     {
         [SerializeField] private string sceneName;
         [SerializeField] private Color  loadToColor = Color.black;
+        [SerializeField] private float  duration    = 1f;
 
         private Button _button;
 
         private void Start()
         {
             _button = GetComponent<Button>();
-            _button.onClick.AddListener(() => { Initiate.Fade(sceneName, loadToColor, 1.0f); });
+            _button.onClick.AddListener(() => { Initiate.Fade(sceneName, loadToColor, duration); });
         }
     }
 }
---
+++ Assets/Scripts/UI/MenuPanel.cs
@@ -0,0 +1,41 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace UI
+{
+    public class MenuPanel : MonoBehaviour
+    {
+        [SerializeField] private RectTransform panel;
+        [SerializeField] private float         duration = 1f;
+
+        private bool _isActive;
+
+        private void Start()
+        {
+            panel.localScale = Vector3.zero;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _isActive = !_isActive;
+
+                if (_isActive)
+                    ShowPopup();
+                else
+                    HidePopup();
+            }
+        }
+
+        private void ShowPopup()
+        {
+            panel.DOScale(Vector3.one, duration);
+        }
+
+        private void HidePopup()
+        {
+            panel.DOScale(Vector3.zero, duration);
+        }
+    }
+}
